ebe17b1a2e18a16aa349e0bed52d5522d0f12cd2
JeffSCXDD <shengchang82+github@gmail.com>
2017-02-13 23:30:38 -0800

    Making chages to owner/group to suit the local user group

--- helloworld/recipes/default.rb
+++ helloworld/recipes/default.rb
@@ -1,8 +1,8 @@
 
       file "/tmp/helloworld.txt" do
-        owner "ubuntu"
-        group "ubuntu"
+        owner "jeff"
+        group "jeff"
         mode 00544
         action :create
-        content "Hello, Implementor!"
+        content "Testing of Running Chef cookbook the first time!"
       end
